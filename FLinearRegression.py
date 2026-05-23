93715b5e42d43ee48dbe9b3f10fde0ca3746d510
zenthic021 <ronixyt07@gmail.com>
2026-05-23 12:55:30 +0530

    Update FLinearRegression.py

--- FLinearRegression.py
+++ FLinearRegression.py
@@ -1,33 +1,59 @@
 import pandas as pd
+
+import numpy as np
+
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 
-df=pd.read_csv('final_dataset.csv')
+from sklearn.linear_model import Linear Regressio
 
-print(df.head())
+np.random.seed (0)
 
-X=df[['size']]
-Y=df['price']
+X=np.random.rand(100,1)
 
-plt.scatter(X,Y,color='green')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title("scatter plot of x and y")
+y=2+3*X+np.random.randn (100,1)
+
+data-pd.DataFrame (
+
+data=np.hstack((X,y)), columns=['X','y'] )
+
+plt.figure(figsize=(8,6))
+
+plt.scatter (data['X'], data['y'])
+
+plt.title("scatter plot of x,y")
+
+plt.xlabel("x")
+
+plt.ylabel("y")
+
 plt.show()
 
-model=LinearRegression()
-model.fit(X,Y)
+correlation=data["X"].corr(data["y"])
 
-y_pred=model.predict(X)
+print (f"correeltaion is: (correlation}")
 
-plt.scatter(X,Y,label='Actual data')
-plt.plot(X,y_pred,label='LinerLine')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title("Linear Regression Model")
-plt.show()
+model-LinearRegression ()
 
+model.fit(X,y)
 
+intercept-model.intercept_[0]
 
+slope-model.coef [0] [0]
 
+print (f"intercept: (intercept)")
 
+print (f"slope: (slope)")
+
+plt.figure(figsize=(8,6))
+
+plt.scatter(data['X'], data['y'])
+
+plt.plot(data['X'], model.predict(X),color='red')
+
+plt.title("LinearRegression model")
+
+plt.xlabel("x")
+
+plt.ylabel("y")
+
+pit.show()
